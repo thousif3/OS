4ac5ea1ab3f868817cff414b811c47cba3d6ac27
thousif3 <116739003+thousif3@users.noreply.github.com>
2023-12-19 16:25:43 +0530

    Update Armstrong.sh

--- Armstrong.sh
+++ Armstrong.sh
@@ -1,17 +1,14 @@
-echo -n "Enter a number: "
-read n
+read -p "Enter a number: " n
 t=$n
 sum=0
 while [ $n -gt 0 ]
-do 
-    rem=$((n%10))
-    cube=$((rem*rem*rem))
-    sum=$((sum+cube))
-    n=$((n/10))
+do
+   sum=$((sum + ((n%10) * (n%10) * (n%10))))  
+   n=$((n/10))
 done
 if [ $sum == $t ]
 then
-    echo "$t is a Armstrong Number"
+   echo "$t is an Armstrong Number"
 else
-    echo "$t is not a Armstrong Number"
+   echo "$t is not an Armstrong Number"
 fi
