3eba74b25619fe6940f095dbdd1aa0318a0fe034
thousif3 <116739003+thousif3@users.noreply.github.com>
2023-12-19 21:27:21 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -1,62 +1,74 @@
 #include <stdio.h>
-
-void findWaitingTime(int processes[], int n, int bt[], int wt[])
-{
-  wt[0] = 0;
-
-  for (int i = 1; i < n; i++)
-    wt[i] = bt[i - 1] + wt[i - 1];
-}
-
-void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[])
-{
-  for (int i = 0; i < n; i++)
-    tat[i] = bt[i] + wt[i];
-}
-
-void findAverageTime(int processes[], int n, int bt[])
-{
-  int wt[1000], tat[1000];
-
-  findWaitingTime(processes, n, bt, wt);
-  findTurnAroundTime(processes, n, bt, wt, tat);
-
-  printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
-
-  float total_wt = 0, total_tat = 0;
-
-  for (int i = 0; i < n; i++)
-  {
-    total_wt += wt[i];
-    total_tat += tat[i];
-
-    printf("%d\t%d\t\t%d\t\t%d\n", processes[i], bt[i], wt[i], tat[i]);
-  }
-
-  printf("\nAverage Waiting Time: %.2f", total_wt / n);
-  printf("\nAverage Turnaround Time: %.2f\n", total_tat / n);
-}
-
 int main()
 {
-  int n;
+    int p[10], at[10], bt[10], ct[10], tat[10], wt[10], i, j, temp = 0, n;
+    float awt = 0, atat = 0;
+    printf("enter no of proccess you want:");
+    scanf("%d", &n);
+    printf("enter %d process:", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &p[i]);
+    }
+    printf("enter %d arrival time:", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &at[i]);
+    }
+    printf("enter %d burst time:", n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &bt[i]);
+    }
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < (n - i); j++)
+        {
+            if (at[j] > at[j + 1])
+            {
+                // swap p
+                temp = p[j + 1];
+                p[j + 1] = p[j];
+                p[j] = temp;
 
-  printf("Enter the number of processes: ");
-  scanf("%d", &n);
+                // swap at
+                temp = at[j + 1];
+                at[j + 1] = at[j];
+                at[j] = temp;
 
-  int processes[1000];
-  int burst_time[1000];
+                // swap bt
+                temp = bt[j + 1];
+                bt[j + 1] = bt[j];
+                bt[j] = temp;
+            }
+        }
+    }
+    ct[0] = at[0] + bt[0];
+    for (i = 1; i < n; i++)
+    {
+        temp = 0;
+        if (ct[i - 1] < at[i])
+        {
+            temp = at[i] - ct[i - 1];
+        }
+        ct[i] = ct[i - 1] + bt[i] + temp;
+    }
+    printf("\np\t A.T\t B.T\t C.T\t TAT\t WT");
+    for (i = 0; i < n; i++)
+    {
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+        atat += tat[i];
+        awt += wt[i];
+    }
+    atat = atat / n;
+    awt = awt / n;
+    for (i = 0; i < n; i++)
+    {
+        printf("\nP%d\t %d\t %d\t %d \t %d \t %d", p[i], at[i], bt[i], ct[i], tat[i], wt[i]);
+    }
+    printf("\naverage turnaround time is %f", atat);
 
-  printf("Enter the burst time for each process:\n");
-
-  for (int i = 0; i < n; i++)
-  {
-    printf("Enter burst time for process %d: ", i + 1);
-    scanf("%d", &burst_time[i]);
-    processes[i] = i + 1;
-  }
-
-  findAverageTime(processes, n, burst_time);
-
-  return 0;
+    printf("\naverage wating timme is %f\n", awt);
+    return 0;
 }
