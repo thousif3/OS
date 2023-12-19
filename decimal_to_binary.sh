e27d5ffe1edc4864025974253b573321ebbc27b7
thousif3 <116739003+thousif3@users.noreply.github.com>
2023-12-19 16:29:33 +0530

    Update decimal_to_binary.sh

--- decimal_to_binary.sh
+++ decimal_to_binary.sh
@@ -1,19 +1,13 @@
-echo "Enter any decimal no:"
-read num
-rem=1
-bno=" "
-while [ $num -gt 0 ]
-do
-rem=`expr $num % 2 `
-bno=$bno$rem
-num=`expr $num / 2 `
+#!/bin/bash
+
+read -p "Enter a decimal number: " num
+
+binary=""
+
+while [[ $num -gt 0 ]]; do
+  remainder=$((num % 2))  
+  binary="${remainder}${binary}"  
+  num=$((num / 2))  
 done
-i=${#bno}
-final=" "
-while [ $i -gt 0 ]
-do
-rev=`echo $bno | awk '{ printf substr( $0,'$i',1 ) }'`
-final=$final$rev
-i=$(( $i - 1 ))
-done
-echo "Equivalent Binary no:" $final 
+
+echo "The binary equivalent is: $binary"
